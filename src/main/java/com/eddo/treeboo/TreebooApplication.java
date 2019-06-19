dc2aefcbdf2940f53147721e1d5200c6ff9301c9
Eddevelit <edd.develitgmail.com>
2019-06-19 00:04:20 -0500

    Se inicia el proyecto

---
+++ src/main/java/com/eddo/treeboo/TreebooApplication.java
@@ -0,0 +1,13 @@
+package com.eddo.treeboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreebooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreebooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eddo/treeboo/TreebooApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eddo.treeboo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreebooApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
